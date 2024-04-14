7554bca3b5c7cb15d08d5c6c9f8c9eed43e74fdf
`COLIN CARTER <VANBLERK666@HOTMAIL.COM>
2024-04-14 19:48:52 +0100

    database added

--- server/seed.js
+++ server/seed.js
@@ -1,8 +1,12 @@
 import Database from "better-sqlite3";
+
+//create or read database file
+
 const db = new Database("database.db");
 
 db.exec(`
-  CREATE TABLE messages (
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     message TEXT
   )
